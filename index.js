a3c337f442a9490582fcbfb16fe219050e7adf98
arun rana <arun0808rana@gmail.com>
2025-03-31 16:37:12 +0530

    fix: route path

--- index.js
+++ index.js
@@ -9,13 +9,17 @@ const PORT = 5000; // You can change the port
 app.use(cors());
 
 // Proxy route
-app.use('/api', createProxyMiddleware({
+app.use('/proxy', createProxyMiddleware({
   target: 'https://partnersv1.pinbot.ai', // 🔁 Target server (the actual API)
   changeOrigin: true,
   pathRewrite: {
-    '^/api': '', // Remove /api from the URL path when forwarding
+    '^/proxy': '', // Remove /proxy from the URL path when forwarding
   },
 }));
+
+app.get('/health', (req, res)=>{
+  res.send("Healthy")
+})
 
 app.listen(PORT, () => {
   console.log(`Proxy server is running on http://localhost:${PORT}`);
